b8bbe5a6bf32d08e48587fae9c5fee690240c3cb
tv <krishna22kanth77@gmail.com>
2026-07-13 13:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("power-btn").addEventListener("click", () => {
-  const confirmExit = confirm("Are you sure you want to exit TvFlix?");
+  const confirmExit = confirm("Exit TvFlix?");
   if (confirmExit) {
-    window.close(); // closes the window (works in some browsers)
-    alert("TvFlix has been exited."); // fallback message
+    window.close();
+    alert("TvFlix closed.");
   }
 });
